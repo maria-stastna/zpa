3cfb8105755ee81d844332260691fa5b5bdb3754
Maria Šťastná <mari@Marias-MacBook-Air.local>
2022-11-02 16:49:34 +0100

    homework3_4

---
+++ exercise3-4.py
@@ -0,0 +1,22 @@
+#3a
+string = '[['
+string1 = 'python'
+string2 = ']]' 
+print (string + string1 + string2)
+
+word = 'python'
+word2 = f'[[{word}]]'
+print (word2)
+
+#3b
+text3 = 'friends'[2:4:1]
+print (text3*3)
+
+#4a
+text = 'friends'
+text2 = 'my dear '.upper()
+print(text2 + text)
+
+#4b
+text4 = 'minions' [0:1:1]
+print (text4 *7)
