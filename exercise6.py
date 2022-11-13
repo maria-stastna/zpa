7fdd0aefa7194b0d0b6da26b2ab29302a937d7b0
Maria Šťastná <mari@Marias-MacBook-Air.local>
2022-11-13 23:25:27 +0100

    add exercise 6

--- exercise6.py
+++ exercise6.py
@@ -1,3 +1,12 @@
 #6a
 txt = "My hobby is {hobby}."
-print (txt.format(hobby = 'drawing'))
+print(txt.format(hobby = 'drawing'))
+
+#6b
+date = '2018-11-01'
+pieces = date.split('-')
+year = pieces[0]
+month = pieces[1]
+day = pieces[2]
+date2 = "{a}/{b}"
+print(date2.format(a=day, b=month))
