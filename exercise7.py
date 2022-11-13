0e226304203cb7b724ffffd3799328595d099849
Maria Šťastná <mari@Marias-MacBook-Air.local>
2022-11-13 22:01:54 +0100

    add exercise 7

---
+++ exercise7.py
@@ -0,0 +1,7 @@
+my_list = ["drawing", "painting", "cooking", "watching movies",]
+my_list.append("making coffee")
+print(my_list)
+print("hobby I like the most: " + my_list[0])
+print("hobby I like the least: " + my_list[-1])
+del my_list[2]
+print(my_list)
